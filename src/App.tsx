68cf5179231a6135dc5005d0515be982a1730a94
DmitryKRTV <dmitrykorotaev.job@gmail.com>
2022-09-13 15:12:58 +0300

    Commit 4. Added Skills components and base breakpoints.

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,19 @@ import "./App.css";
 import {Header} from "./components/Header/Header";
 import {Home} from "./components/Home/Home";
 import {About} from "./components/About/About";
+import {Skills} from "./components/Skills/Skills";
 
 function App() {
     return (
-        <div className="App">
-            <>
-                <Header/>
-                <main className="main">
-                    <Home/>
-                </main>
-                <About/>
-            </>
-        </div>
+        <>
+            <Header/>
+            <main className="main">
+                <Home/>
+            </main>
+            <About/>
+            <Skills/>
+        </>
+
     );
 }
 
