7f1ea2bdd18a36823699bfbdc8d6d81350405d4f
DmitryKRTV <dmitrykorotaev.job@gmail.com>
2022-09-14 17:06:39 +0300

    Commit 9. Added Footer component and base breakpoints.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {Services} from "./components/Services/Services";
 import {Qualification} from "./components/Qualification/Qualification";
 import {Testimonials} from "./components/Testimonials/Testimonials";
 import {Contact} from "./components/Contact/Contact";
+import {Footer} from "./components/Footer/Footer";
 
 function App() {
     return (
@@ -22,6 +23,7 @@ function App() {
             <Qualification/>
             <Testimonials/>
             <Contact/>
+            <Footer/>
         </>
 
     );
